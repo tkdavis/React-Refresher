4309dc19cbb30d78109fe0ad393b45be48261e3a
Tyler Davis <datadazer@gmail.com>
2020-01-28 12:44:33 -0800

    Add card-list component and get json of user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -7,21 +7,28 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello Tyler'
+      monsters: []
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
   render() {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              {this.state.string}
-            </p>
-            <button onClick={() => this.setState({string: 'Hello Andrea'})}>Change Text</button>
-          </header>
-        </div>
-      );
+    return (
+      <div className="App">
+        <CardList name="Ty">
+          {
+            this.state.monsters.map(monster => {
+              return <h1 key={monster.id}>{monster.name}</h1>
+            })
+          }
+        </CardList>
+      </div>
+    );
   }
 }
 
