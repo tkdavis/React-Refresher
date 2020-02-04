7082b90f417726663813cb68e212109cfe949023
Tyler Davis <datadazer@gmail.com>
2020-02-04 13:03:26 -0800

    Breakout searchbox into separate functional component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
@@ -10,6 +11,10 @@ class App extends Component {
       monsters: [],
       searchField: ''
     }
+  }
+
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
   }
 
   componentDidMount() {
@@ -25,7 +30,7 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <input type='search' placeholder='search monsters' onChange= {e => this.setState({searchField: e.target.value})}/>
+        <SearchBox placeholder='search monsters' handleChange={this.handleChange}/>
         <CardList monsters={filteredMonsters}/>
       </div>
     );
